f8a9cb4f45014d31cacdc07ecedf909d82f9ebce
pedrogawa <phfbarreiros95@gmail.com>
2020-04-07 14:36:04 -0300

    Desenvolvimento dos métodos POST, GET, PUT e DELETE no código.

--- src/app.js
+++ src/app.js
@@ -1,33 +1,95 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+//{ id: "uuid", title: 'Desafio Node.js', techs: ["Node.js", "..."], likes: 0 };
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories)
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, techs, likes } = request.body;
+
+  const repository = { id: uuid(), title, techs, likes };
+
+  repositories.push(repository);
+
+  return response.json(repository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, techs, likes } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Project not found!' })
+  }
+
+  const repository = {
+    id,
+    title,
+    techs,
+    likes,
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository)
+
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Project not found!' })
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, techs, likes } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Project not found!' })
+  }
+
+  const repository = {
+    id,
+    title,
+    techs,
+    likes,
+  }
+
+  
+  repositories[repositoryIndex].likes += 1;
+  repositories[repositoryIndex] = repository;
+
+
+  return response.json(repository.likes)
+
 });
 
 module.exports = app;
